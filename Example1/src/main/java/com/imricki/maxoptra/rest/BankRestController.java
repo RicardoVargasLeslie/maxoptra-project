440e794f905f6b2d557b716eb3e6fb8efaf22d37
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-07-25 22:18:54 +0100

    setting up the view

--- Example1/src/main/java/com/imricki/maxoptra/rest/BankRestController.java
+++ Example1/src/main/java/com/imricki/maxoptra/rest/BankRestController.java
@@ -2,13 +2,14 @@ package com.imricki.maxoptra.rest;
 
 import java.util.logging.Logger;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController
+@Controller
 public class BankRestController {
 
 	private static final Logger LOGGER = Logger.getLogger(BankRestController.class.getName());
@@ -19,9 +20,20 @@ public class BankRestController {
 	}
 
 	@PostMapping("/upload-csv-file")
-	public String uploadCSVFile(@RequestParam("file") MultipartFile file) {
+	public String uploadCSVFile(@RequestParam("file") MultipartFile file, Model model) {
 
-		return "";
+		// validate file
+		if (file.isEmpty()) {
+			model.addAttribute("message", "Please select a CSV file to upload.");
+			model.addAttribute("status", false);
+		} else {
+			// save users list on model
+			model.addAttribute("users", users);
+			model.addAttribute("status", true);
+
+		}
+
+		return "file-upload-status";
 	}
 
 }
