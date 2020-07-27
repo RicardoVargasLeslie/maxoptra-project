1379498abe02c2fd0ce410bcb3a5d1c5d678eeff
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-07-27 17:06:36 +0100

    done logic

--- Example2/src/main/java/com/imricki/maxoptra/app/App.java
+++ Example2/src/main/java/com/imricki/maxoptra/app/App.java
@@ -5,5 +5,28 @@ public class App {
 	
 	public static void main(String[] args) {
 		
+		Integer num=17;
+		
+		if (num == (int)num)
+		{
+			operation(num);
+		}
+		
 	}
+	
+	
+	public  static void operation(int number) {
+		
+		Integer sumOfNumbers = 0;
+		
+	        
+	        while (number > 0) {
+	            sumOfNumbers = sumOfNumbers + number % 10;
+	            number = number / 10;
+	        }
+	      
+	        
+	}
+	
+	
 }
