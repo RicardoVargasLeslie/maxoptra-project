d5c56b9116becc39dfe76209a01aa60bf012e9a6
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-07-27 17:16:00 +0100

    minor refactor

--- Example2/src/main/java/com/imricki/maxoptra/app/App.java
+++ Example2/src/main/java/com/imricki/maxoptra/app/App.java
@@ -1,32 +1,34 @@
 package com.imricki.maxoptra.app;
 
+/**
+ * 
+ * @author Ricardo Vargas
+ * @version 1.0
+ *
+ */
 public class App {
 
-	
 	public static void main(String[] args) {
-		
-		Integer num=17;
-		
-		if (num == (int)num)
-		{
-			operation(num);
+
+		Integer num = 17;
+
+		if (num == (int) num) {
+			System.out.println("The number is: " + operation(num));
 		}
-		
+
 	}
-	
-	
-	public  static void operation(int number) {
-		
+
+	public static Integer operation(int numberInput) {
+
 		Integer sumOfNumbers = 0;
-		
-	        
-	        while (number > 0) {
-	            sumOfNumbers = sumOfNumbers + number % 10;
-	            number = number / 10;
-	        }
-	      
-	        
+
+		while (numberInput > 0) {
+			sumOfNumbers = sumOfNumbers + numberInput % 10;
+			numberInput = numberInput / 10;
+		}
+
+		return sumOfNumbers;
+
 	}
-	
-	
+
 }
