2809dfb3b9a7723bd37591157087c4a8cec20f70
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-07-28 23:11:24 +0100

    clean up

--- Example1/src/test/java/com/imricki/maxoptra/Example1/BankDetailTest.java
+++ Example1/src/test/java/com/imricki/maxoptra/Example1/BankDetailTest.java
@@ -1,31 +1,24 @@
 package com.imricki.maxoptra.Example1;
 
-
-
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
-import static org.mockito.ArgumentMatchers.any;
+
 import java.util.ArrayList;
-import org.junit.Before;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-
+import org.mockito.junit.MockitoJUnitRunner;
 
 import com.imricki.maxoptra.dto.BankDetailDto;
-
-
 import com.imricki.maxoptra.service.ProcesData;
 
 @RunWith(MockitoJUnitRunner.class)
- class BankDetailTest {
+class BankDetailTest {
 
 	@InjectMocks
 	private ProcesData service;
@@ -45,21 +38,21 @@ import com.imricki.maxoptra.service.ProcesData;
 
 	@Test
 	void shouldAddAnewDetail() {
-		
+
 		// Given ----------------------------//
-		
+
 		final String bankInput = "HSBC Canada";
 		final String cardNumberInput = "5601-2345-3446-5678";
 		final String expirydateInput = "Nov-2017";
-		
+
 		// ---------------------------------//
 		when(dtoMock.getBank()).thenReturn(bankInput);
 		when(dtoMock.getCardNumber()).thenReturn(cardNumberInput);
 		when(dtoMock.getExpirydate()).thenReturn(expirydateInput);
-		
+
 		// When ----------------------------//
 		service.addDetails(dtoMock);
-	
+
 		// Then ----------------------------//
 		InOrder inOrder = Mockito.inOrder(dtoMock, listMock);
 		inOrder.verify(listMock).add(dtoMock);
@@ -67,24 +60,23 @@ import com.imricki.maxoptra.service.ProcesData;
 
 	}
 
-
 //	@Test
 //	void shouldDoNothinBadDetails() {
-//		
+//
 //		//Given
-//		
+//
 //		BankDetailDto badDetail=null;
 //		// -------------------------------------//
-//		 
+//
 //		when(dto).then(null);
-//		
+//
 //		// When ----------------------------------//
 //		service.addDetails(dto);
-//		
+//
 //		// Then -----------------------------------//
 //		InOrder InOrder = Mockito.inOrder(dto);
 //		InOrder.verify(dto).getBank().equals(null);
-//		
+//
 //		InOrder.verifyNoMoreInteractions();
 ////		// ---------------------------------//
 //	}
