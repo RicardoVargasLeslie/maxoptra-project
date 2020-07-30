e11c09b5f8b8a49c98636a4e6a18966191490f5c
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-07-30 15:28:03 +0100

    testing controller

--- Example1/src/test/java/com/imricki/maxoptra/api/BankControllerTest.java
+++ Example1/src/test/java/com/imricki/maxoptra/api/BankControllerTest.java
@@ -1,28 +1,37 @@
 package com.imricki.maxoptra.api;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.ui.Model;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
 class BankControllerTest {
 
-	@InjectMocks
-	BankController api;
+	MockMvc mockMvc;
 
-	@Mock
-	Model model;
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	@BeforeEach
+	public void setUp() {
+
+		MockitoAnnotations.initMocks(this);
+
+		// mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
 
 	@Test
-	void shouldShowform() {
+	void shouldLoadIndex() throws Exception {
+
+		// mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
 
 	}
 
-//	@GetMapping("/addForm")
-//	public String showAddForm(Model model) {
-//		LOGGER.info("showAddUpForm() ----> index()");
-//
-//		model.addAttribute("bankDetailDto", new BankDetailDto());
-//		return "new";
-//	}
 }
