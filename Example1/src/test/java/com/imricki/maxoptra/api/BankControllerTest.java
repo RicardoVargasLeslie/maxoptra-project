9463b8936c6dca9adb3427288d7df96252d1143c
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-07-30 15:41:32 +0100

    testing some things in controller

--- Example1/src/test/java/com/imricki/maxoptra/api/BankControllerTest.java
+++ Example1/src/test/java/com/imricki/maxoptra/api/BankControllerTest.java
@@ -2,35 +2,32 @@ package com.imricki.maxoptra.api;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
-@WebAppConfiguration
-@RunWith(SpringJUnit4ClassRunner.class)
 class BankControllerTest {
 
-	MockMvc mockMvc;
-
-	@Autowired
-	WebApplicationContext webApplicationContext;
+//	@Autowired
+//	MockMvc mockMvc;
+//
+//	@Autowired
+//	ProcesData service;
 
 	@BeforeEach
 	public void setUp() {
 
 		MockitoAnnotations.initMocks(this);
 
-		// mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
 
 	@Test
 	void shouldLoadIndex() throws Exception {
 
-		// mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
+//		RequestBuilder request = get("/");
+//		MvcResult result = mockMvc.perform(request).andReturn();
+//
+//		Assert.assertEquals("index", result.getResponse().getContentAsString());
+//
+//		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
 
 	}
 
