c8ff069122f7a5c6178ba32ea2cc504aeee1878e
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-08-07 18:32:58 +0100

    added configuration to maven plugin and cleand up comand line runner

--- Example1/src/main/java/com/imricki/maxoptra/app/Example1Application.java
+++ Example1/src/main/java/com/imricki/maxoptra/app/Example1Application.java
@@ -1,26 +1,13 @@
 package com.imricki.maxoptra.app;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = { "com" })
 public class Example1Application {
-
-//	@Autowired
-//	private ProcesData procesData;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Example1Application.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner run() throws Exception {
-		return args -> {
-
-//			System.err.println(procesData.ProcessList(new File("test.csv")));
-
-		};
-	}
 }
